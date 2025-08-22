de6a27142eb4db8380fd2893463af16cd6f2c296
lunoxdev <lunox.code@gmail.com>
2025-08-21 20:47:06 -0600

    add avatar profile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import GameGrid from "./components/GameGrid";
 import HeaderBar from "./components/HeaderBar";
 import Background from "./components/Background";
 import Carousel from "./components/Carousel";
+import Muzan from "./assets/muzan.webp";
 
 function App() {
   const [games, setGames] = useState<Games[]>(gamesData);
@@ -34,9 +35,22 @@ function App() {
     <div className="mx-auto w-full sm:w-7xl">
       <Background />
       <header>
-        <h1 className="my-2 sm:my-4 text-4xl sm:text-5xl font-semibold">
-          CLASSIC CASINO
-        </h1>
+        {/* Hero */}
+        <section className="flex my-0 sm:my-6">
+          <div className="relative h-auto w-24 md:w-28 overflow-hidden rounded-xl p-1.5 border border-gray-800 mr-4">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#0d4f1c_0%,#2dd138_50%,#0d4f1c_100%)]" />
+            <img
+              className="relative w-full h-full rounded-sm object-cover"
+              src={Muzan}
+              alt="Avatar"
+              loading="eager"
+            />
+          </div>
+          <div className="text-start">
+            <p className="text-lg font-semibold">Muzan Kibutsuji</p>
+            <h1 className="text-2xl font-bold">$1000</h1>
+          </div>
+        </section>
 
         <Carousel games={gamesData} onGamePlay={handleGamePlay} />
 
