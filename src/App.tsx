abf8c836cf626495f1f4c46b4af9754c6b829cfb
lunoxdev <lunox.code@gmail.com>
2025-08-25 12:29:39 -0600

    add muzan video profile

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import GameGrid from "./components/GameGrid";
 import HeaderBar from "./components/HeaderBar";
 import Background from "./components/Background";
 import Carousel from "./components/Carousel";
-import Muzan from "./assets/muzan.webp";
+import muzanVideo from "./assets/muzan-video.mp4";
 
 function App() {
   const [games, setGames] = useState<Games[]>(gamesData);
@@ -35,24 +35,27 @@ function App() {
     <div className="mx-auto w-full sm:w-7xl flex">
       <Background />
       <aside className="hidden lg:block w-60 bg-green-800/50 rounded-xl p-4 mr-6">
-        <h1 className="animatimate-text-gradient text-5xl font-bold">CASINO</h1>
+        <h1 className="animatimate-text-gradient bg-gradient-to-r from-[#0d4f1c] via-[#2dd138] via-50% to-[#0d4f1c] text-5xl font-bold">
+          CASINO
+        </h1>
       </aside>
       <div className="flex-1">
         <header>
           {/* Hero */}
           <section className="flex my-0 sm:my-6">
-            <div className="relative h-auto w-24 md:w-28 overflow-hidden rounded-xl p-1.5 border border-gray-800 mr-4">
+            <div className="relative h-20 w-20 overflow-hidden rounded-full p-1 border border-gray-800 mr-4">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#0d4f1c_0%,#2dd138_50%,#0d4f1c_100%)]" />
-              <img
-                className="relative w-full h-full rounded-sm object-cover"
-                src={Muzan}
-                alt="Avatar"
-                loading="eager"
+              <video
+                src={muzanVideo}
+                autoPlay
+                loop
+                muted
+                className="relative w-full h-full rounded-full object-cover"
               />
             </div>
             <div className="flex flex-col text-start">
               <span className="text-lg font-semibold">Muzan Kibutsuji</span>
-              <span className="animatimate-text-gradient text-2xl font-bold">
+              <span className="animatimate-text-gradient bg-gradient-to-r from-[#8b4513] via-[#ffd700] via-50% to-[#8b4513] text-2xl font-bold">
                 $1000
               </span>
             </div>
